b1fba99d42fd10d921c9f197ea7d6316259d8435
Diego <diego@local.com>
2022-01-25 18:18:25 +0100

    script listo para usar con archivo "encuestas.csv".

--- Encuestas.R
+++ Encuestas.R
@@ -1,10 +1,8 @@
-setwd("C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/2021 tablas")
-library(readODS)
+setwd("C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/encuestas")
 library(dplyr)
-##
 
-encuestas <- read_ods("multidim_quests_bra_arg_ecu.ods")   #incluye cargas de datos en proceso
-encuestas <- encuestas[1:111,] #solo estudios cargados completamente
+encuestas <- read.csv(file = 'encuestas.csv') #script armado partiendo de la tabla con todos los estudios.
+#al final del script: manejo de tablas para GitHub.
 
 ### Biodiversity Friendly Practices ###
 dataBFP <- encuestas %>% select(n_crops_tot,wild_veg_edge_incrop,wild_area_property,
@@ -15,12 +13,11 @@ crops_01z <- (dataBFP$n_crops_tot-1)#resto 1: paso "1 cultivo" a categor?a 0. Y 
 dataBFP$crops_01[crops_01z>0] <-1
 dataBFP$crops_01[crops_01z==0] <-0  #hay q aclararle, si no pone NA. 
 
-
 #pestic_chem_appl_year to binary: yes or no pesticide
 dataBFP$pestic_01[dataBFP$pestic_chem_appl_year>0] <-1
 dataBFP$pestic_01[dataBFP$pestic_chem_appl_year==0] <-0 #hay q aclararle, si no pone NA. 
 
-### Build BFP variable. With all binary 0-1 ###
+## Build BFP variable. With all binary 0-1 
 colnames(dataBFP)
 
 #all BFP (all in apple Arg):
@@ -34,51 +31,54 @@ dataBFP <- dataBFP %>% mutate(BFP1= crops_01 + pestic_01+
 #all in beans Ecu:
 dataBFP <- dataBFP %>% mutate(BFP2= crops_01+pestic_che0_org1+fertil_org1_ino0)
 
-dataBFP$study <- encuestas$study  #incluyo indicador de estudio.
+dataBFP$study_code <- encuestas$study_code  #incluyo indicador de estudio.
 
-### Select predictors ###  *(por ahora conservation actions qued? afuera de ambas)
+
+### Select predictors ###  *(por ahora conservation actions quedo afuera de ambas)
 colnames(encuestas)
-predictors <- encuestas %>% select(study, owner, education, interact_w_prof, gender_f1_m0,	
+predictors <- encuestas %>% select(study_code, owner, education, interact_w_prof, gender_f1_m0,	
                                machin_s0_h1,	assoc_inst, other_income,hive_use, commerce,
                                access_credit,	irrigation,	family_tot_n,	family_female_n,	
                                family_male_n, bee_importance, age, internet_access,	certific,
                                employee_tot_n,	employee_male_n, employee_female_n, 
                                crops_area, property_ha, activs_field0_admin1_both2)
 
+
 ### Regression trees ### 
 #subset per study
 
 #Alto Valle _AV
-dataBFP_AV <- filter(dataBFP, study == "alto_valle")
-predictors_AV <- filter(predictors, study == "alto_valle")
+dataBFP_AV <- filter(dataBFP, study_code == "AV") %>% select(-study_code)
+predictors_AV <- filter(predictors, study_code == "AV") %>% select(-study_code)
 #arrange data for RT for Alto Valle
 BFP0 <- dataBFP_AV$BFP0   #response var
-RT_AV <- cbind(BFP0, predictors_AV)
+RT_AV <- cbind(BFP0, predictors_AV) %>% filter(!is.na(BFP0)) #junto predics con rta, y elimino NAs en respuesta (para party-ctree).
+
 
 #Coffe _CBH
-dataBFP_CBH <- filter(dataBFP, study == "coffe_bahia")
-predictors_CBH <- filter(predictors, study == "coffe_bahia")
+dataBFP_CBH <- filter(dataBFP, study_code == "CBH")%>% select(-study_code)
+predictors_CBH <- filter(predictors, study_code == "CBH")%>% select(-study_code)
 #arrange data for RT for coffe_bahia
 BFP1 <- dataBFP_CBH$BFP1   #response var
 RT_CBH <- cbind(BFP1, predictors_CBH)
 
 #habas_cotopaxi
-dataBFP_HCO <- filter(dataBFP, study == "habas_cotopaxi")
-predictors_HCO <- filter(predictors, study == "habas_cotopaxi")
+dataBFP_HCO <- filter(dataBFP, study_code == "HCO")%>% select(-study_code)
+predictors_HCO <- filter(predictors, study_code == "HCO")%>% select(-study_code)
 #arrange data for RT for coffe_bahia
 BFP2 <- dataBFP_HCO$BFP2   #response var
 RT_HCO <- cbind(BFP2, predictors_HCO)
 
-
+### rPART
 library(rpart)
-# grow tree
 
+# grow tree
 #Alto Valle _AV
 fit <- rpart(BFP0~owner+ education+ interact_w_prof+ gender_f1_m0+	
                machin_s0_h1+	assoc_inst+ other_income+hive_use+ commerce+
                access_credit+	irrigation+	family_tot_n+	family_female_n+	
-               family_male_n+  bee_importance+ age+ internet_access+	certific+   #
-               employee_tot_n+	employee_male_n+ employee_female_n+ 
+               family_male_n+ bee_importance+ age+ internet_access+	certific+   #
+               employee_tot_n+employee_male_n+ employee_female_n+ 
                crops_area+ property_ha+ activs_field0_admin1_both2, 
              method="anova", data=RT_AV)
 #Coffe _CBH
@@ -86,7 +86,7 @@ fit <- rpart(BFP1~owner+ education+ interact_w_prof+ gender_f1_m0+
                machin_s0_h1+	assoc_inst+ other_income+hive_use+ commerce+
                access_credit+	irrigation+	family_tot_n+	family_female_n+	
                family_male_n+ bee_importance+ age+ internet_access+	certific+   #
-               employee_tot_n+	employee_male_n+ employee_female_n+ 
+               employee_tot_n+employee_male_n+ employee_female_n+ 
                crops_area+ property_ha+ activs_field0_admin1_both2, 
              method="anova", data=RT_CBH)
 #habas_cotopaxi
@@ -98,7 +98,7 @@ fit <- rpart(BFP2~owner+ education+ interact_w_prof+ gender_f1_m0+
                crops_area+ property_ha+ activs_field0_admin1_both2, 
              method="anova", data=RT_HCO)
 
-#Resulatados
+#Resultados
 plot(fit)
 printcp(fit) # display the results
 plotcp(fit) # visualize cross-validation results
@@ -108,12 +108,29 @@ par(mfrow=c(1,2)) # two plots on one page
 rsq.rpart(fit) # visualize cross-validation results  
 
 
-### PARTY...
-install.packages("multcomp")
+### PARTY
 install.packages("party")
 library(party)
-fit <- ctree(BFP0~., data=RT_AV)
+fit <- ctree(BFP0~., data=RT_AV)   #VER lo del character...
+plot(fit)
 
 
 
+
+
+
+
+
+#bases para GitHub:
+Apple_arg <- filter(encuestas, study_code == "AV")
+Coffe_bra <- filter(encuestas, study_code == "CBH")
+Beans_ecu <- filter(encuestas, study_code == "HCO")
+write.csv(Apple_arg,"C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/encuestas\\Apple_arg.csv", row.names = FALSE)
+write.csv(Coffe_bra,"C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/encuestas\\Coffe_bra.csv", row.names = FALSE)
+write.csv(Beans_ecu,"C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/encuestas\\Beans_ecu.csv", row.names = FALSE)
+
+# tabla completa. falta SEGUIR AGREGANDO estudios. 
+encuestas1 <- read_ods("encuestas_full.ods")   #incluye cargas de datos en proceso
+encuestas1 <- encuestas[1:111,]              #solo estudios cargados completamente.
+write.csv(encuestas1,"C:/Users/diego/Dropbox/IRNAD/OBServ/EBD/encuestas\\encuestas.csv", row.names = FALSE)
 
